fe1f06e61b0113768d7b32d16f0d356bc12aaf87
tanmaykk9 <tanmaykamath92@gmail.com>
2020-04-30 22:40:23 +0530

    2 files modified

--- src/app/keylogger.directive.ts
+++ src/app/keylogger.directive.ts
@@ -6,16 +6,55 @@ import {KEYCODE} from './key-code.enum';
 })
 export class KeyloggerDirective {
 
-  constructor() { }
+  constructor() { 
+
+  }
+
 
   @HostListener('window:keyup', ['$event'])
+ 
   keyEvent(event: KeyboardEvent) {
-    console.log(event);
-     if (event.keyCode === KEYCODE.RIGHT_ARROW) {
-      console.log("Right Arrow Clicked");
+    var target = event.target;
+    // Can be Dynamic, But Static for flexibility
+    var logKeyEvent = "[KEYPRS]";
+    var logBtnEvent = "[BUTTON]";
+
+    // Current Time to Print as per requirement
+    Date.prototype.timeNow = function () {
+      // Inherited Date Time Object to print Only Time as per requirement
+      return ((this.getHours() < 10)?"0":"") + this.getHours() +":"+ ((this.getMinutes() < 10)?"0":"") + this.getMinutes() +":"+ ((this.getSeconds() < 10)?"0":"") + this.getSeconds();
     }
-      if (event.keyCode === KEYCODE.LEFT_ARROW) {
-      console.log("Left arrow Clicked");
+    var newDate = new Date();
+    var datetime = newDate.timeNow();
+     //debugger
+    if(target.tagName === "BODY") {
+      event.preventDefault();
+    } 
+    if(target.tagName === "INPUT") {
+      if(event.type === "keyup") {
+        // var date = new Date().getDate().toString();
+        // var month = new Date().getMonth().toString();
+        // var year = new Date().getFullYear().toString();
+        // var printDate = '[' + date + '-' + month + '-' + year + ']';
+  
+        console.log(datetime + ' - ' + logKeyEvent + ' - ' + event.key + ' key pressed');
+      }
+
+    //  if (event.keyCode === KEYCODE.RIGHT_ARROW) {
+    //   console.log("Right Arrow Clicked");
+    // }
+    //   if (event.keyCode === KEYCODE.LEFT_ARROW) {
+    //   console.log("Left arrow Clicked");
+    // }
+    } 
+    if(target.tagName === "BUTTON") {
+      // console.log("Mix Drink pressed");
+      if(event.code === "Enter") {
+        //console.log("Button Pressed");
+        console.log(datetime + ' - ' + logBtnEvent + ' - ' + target.innerText + ' Button has been clicked');
+      } else {
+        event.preventDefault();
+      }
     }
   } 
 }
